e9e0fdfcefb2b2ef26747e69f1f8a3f28702b3e9
Annika Rantala <annika.rantala@helsinki.fi>
2022-01-26 18:51:21 +0200

    #10 Cleaned up extra files

--- naehae.py
+++
@@ -1,8 +0,0 @@
-from urllib import request
-from bs4 import BeautifulSoup
-
-url = "http://www.hs.fi"
-html = request.urlopen(url).read().decode('utf8')
-soup = BeautifulSoup(html, 'html.parser')
-
-print(soup.find_all('span'))
