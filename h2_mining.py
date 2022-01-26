068d84cefd4321c3050f89cd1ad0347bcb27def2
Ines Fröjdö <ines.frojdo@gmail.com>
2022-01-26 18:18:38 +0200

    extracting headlines work!

--- h2_mining.py
+++ h2_mining.py
@@ -6,5 +6,12 @@ link = "https://www.hs.fi"
 html = requests.get(link).text
 soup = BeautifulSoup(html, "html.parser")
 
-for tag in soup.find_all(re.compile("h2")):
-    print(tag)
+#for tag in soup.find_all(re.compile("h2")):
+#    print(tag)
+
+headlines = soup.find_all('h2',{'class':'teaser-title-40'})
+for i in range(len(headlines)):
+    print(headlines[i].text)
+headlines_2 = soup.find_all('h2',{'class':'teaser-title-30'})
+for i in range(len(headlines_2)):
+    print(headlines_2[i].text)
