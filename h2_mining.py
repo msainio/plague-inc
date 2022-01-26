1be7669b0a17eaf0e1d111f7024b4747731daa1c
Ines Fröjdö <ines.frojdo@gmail.com>
2022-01-26 18:28:58 +0200

    #7 changed to helsinginuutiset.fi

--- h2_mining.py
+++ h2_mining.py
@@ -2,16 +2,13 @@ import requests
 import re
 
 from bs4 import BeautifulSoup
-link = "https://www.hs.fi"
+link = "https://www.helsinginuutiset.fi"
 html = requests.get(link).text
 soup = BeautifulSoup(html, "html.parser")
 
-#for tag in soup.find_all(re.compile("h2")):
-#    print(tag)
+headlines = soup.find_all('h2',{'class':'card__title'})
 
-headlines = soup.find_all('h2',{'class':'teaser-title-40'})
-for i in range(len(headlines)):
-    print(headlines[i].text)
-headlines_2 = soup.find_all('h2',{'class':'teaser-title-30'})
-for i in range(len(headlines_2)):
-    print(headlines_2[i].text)
+for x in range(len(headlines)):
+    print(headlines[x].text)
+    print()
+
