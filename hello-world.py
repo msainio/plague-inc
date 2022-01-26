f544877f0198b99dc071f1616ad023f44744184e
alatokartano <aava.latokartano@helsinki.fi>
2022-01-26 14:17:50 +0200

    wrote stuff about plague doctors

--- hello-world.py
+++ hello-world.py
@@ -1,3 +1,4 @@
 print("Ahoy!")
 print("^-^")
-print("The Plague Doctors are here to help you / Ruttotohtorit ovat valmiina auttamaan teitä")
+print("The Plague Doctors are here in Paris to help you with your issues / Ruttotohtorit ovat täällä Pariisissa valmiina auttamaan teitä ongelmissanne")
+
