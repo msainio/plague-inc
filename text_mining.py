c3bcf3bc59e8cf71397f5a9fb64185cac6f3308c
Annika Rantala <annika.rantala@helsinki.fi>
2022-01-26 16:34:53 +0200

    Working on the text mining code

--- text_mining.py
+++ text_mining.py
@@ -1,4 +1,6 @@
+import requests
 from bs4 import BeautifulSoup
-url = "hs.fi"
-html = urlopen(url).read()
+link = "https://www.hs.fi"
+html = requests.get(link).text
 soup = BeautifulSoup(html, "html.parser")
+print(soup.get_text())
