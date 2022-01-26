4206ce363d450446cecf91cb19ece492ed1bdfd4
Annika Rantala <annika.rantala@helsinki.fi>
2022-01-26 18:43:11 +0200

    #8 Added print statements

--- h2_mining.py
+++ h2_mining.py
@@ -8,6 +8,8 @@ soup = BeautifulSoup(html, "html.parser")
 
 headlines = soup.find_all('h2',{'class':'card__title'})
 
+print("Below are the results of our mining: News headlines extracted from helsinginuutiset.fi")
+print("")
 for x in range(len(headlines)):
     print(headlines[x].text)
     print()
