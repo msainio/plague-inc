288de7c98f0c05409439ba589eaba471e30d53e9
Ines Fröjdö <ines.frojdo@gmail.com>
2022-02-02 14:24:03 +0200

    #18 merged the splitting code from split.py to the proper search engine

--- search_engine.py
+++ search_engine.py
@@ -5,17 +5,24 @@ from sklearn.feature_extraction.text import CountVectorizer
 
 # Here we define the data set used in the search engine.
 try:
-    documents = ["This is a silly example",
-                 "A better example",
-                 "Nothing to see here",
-                 "This is a great and long example"]
+   # documents = ["This is a silly example",
+   #              "A better example",
+   #              "Nothing to see here",
+   #              "This is a great and long example"]
+    document = open("enwiki-corpus.txt", "r")
+    corpus = document.read().replace('\n', ' ').replace('>', '')
+    document.close()
+    corpus_list = corpus.split("</article")
+    #print(corpus_list)
+
+
 except FileNotFoundError:
     print("One or more of the input documents was not found.")
 
 # Here we define some useful variables and create a matrix out of the data set.
 
 cv = CountVectorizer(lowercase=True, binary=True, token_pattern=r"(?u)\b\w+\b") # token pattern set to one alphanumeric
-sparse_matrix = cv.fit_transform(documents)
+sparse_matrix = cv.fit_transform(corpus_list)
 sparse_td_matrix = sparse_matrix.T.tocsr()
 t2i = cv.vocabulary_
 
@@ -52,7 +59,7 @@ while query != "":
         for i, doc_idx in enumerate(hits_list):
             if i > (num_matches-1): # limits the amount of matches displayed
                 break
-            print("Match {:d}: {:s}".format(i+1, documents[doc_idx][0:99])) # sets the amount of characters to be displayed
+            print("Match {:d}: {:s}".format(i+1, corpus_list[doc_idx][15:114])) # sets the amount of characters to be displayed
         print()
 
     except:
