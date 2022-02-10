89e295351297a530022d9aec65214402a5b44aae
Ines Fröjdö <ines.frojdo@gmail.com>
2022-02-10 17:27:38 +0200

    made the corpus our set of data

--- user_interface/demo_plague.py
+++ user_interface/demo_plague.py
@@ -1,15 +1,39 @@
 from flask import Flask, render_template, request
 
+# i (ines) just copied all the imports from the relevance-ranked search engine, we probably don't need all of them
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
+import math
+import numpy as np
+import requests
+import re
+from bs4 import BeautifulSoup
+
 #Initialize Flask instance
 app = Flask(__name__)
 
-example_data = [
-    {'name': 'Cat sleeping on a bed', 'source': 'cat.jpg'},
-    {'name': 'Misty forest', 'source': 'forest.jpg'},
-    {'name': 'Bonfire burning', 'source': 'fire.jpg'},
-    {'name': 'Old library', 'source': 'library.jpg'},
-    {'name': 'Sliced orange', 'source': 'orange.jpg'}
-]
+url = "../../data/enwiki-1000-corpus.txt"
+
+corpus_list = []
+name_list = []
+list = []
+
+try:
+    document = open(url, "r")
+    corpus = document.read().replace('\n', ' ')
+    document.close()
+    soup = BeautifulSoup(corpus, "html.parser")
+    for article in soup.find_all('article'):
+        corpus_list.append(article.contents.pop())
+        name_list.append(article.get('name'))
+
+except FileNotFoundError:
+    print("One or more of the input documents was not found.")
+
+# create a list of dictionaries where {'article': xxx, 'content': yyy} as per the miau1 example
+for i,j in zip(name_list, corpus_list):
+    list.append({'article': i, 'content': j})
+
 
 #Function search() is associated with the address base URL + "/search"
 @app.route('/search')
@@ -24,9 +48,9 @@ def search():
     #If query exists (i.e. is not None)
     if query:
         #Look at each entry in the example data
-        for entry in example_data:
+        for entry in list:
             #If an entry name contains the query, add the entry to matches
-            if query.lower() in entry['name'].lower():
+            if query.lower() in entry['article'].lower():
                 matches.append(entry)
 
     #Render index.html with matches variable
