ef3877f3ebb4844443a763d23c14b84ccfc037c6
Ines Fröjdö <ines.frojdo@gmail.com>
2022-02-28 15:43:16 +0200

    #28 join()ed the all lines per movie into one list element

--- plague_project/corpus_prep.py
+++ plague_project/corpus_prep.py
@@ -1,8 +1,8 @@
 
 import re
 
-F_LINES = "../data/movies/movie_lines.txt"
-F_TITLES = "../data/movies/movie_titles_metadata.txt"
+F_LINES = "data/movies/movie_lines.txt"
+F_TITLES = "data/movies/movie_titles_metadata.txt"
 
 # the structure of movie_lines.txt is: line (L), conversation (u), movie title (m), charachter (NAME), line (str)
 fl = open(F_LINES, "r", encoding="ISO-8859-2")
@@ -20,8 +20,8 @@ def prep():
     for item in lines:
         new_item = re.sub(r'[\+$]{7} ', '', item)
         new_m = re.sub(r'L\d{1,6} u\d{1,4} (m\d{1,3}) .+', r'\1', new_item)
-        new_c = re.sub(r'L\d{1,6} u\d{1,4} m\d{1,3} (\w+) .+', r'\1', new_item)
-        new_l = re.sub(r'L\d{1,6} u\d{1,4} m\d{1,3} \w+ (.+)', r'\1', new_item)
+        new_c = re.sub(r'L\d{1,6} u\d{1,4} m\d{1,3} ([A-Z\s]+) .+', r'\1', new_item)
+        new_l = re.sub(r'L\d{1,6} u\d{1,4} m\d{1,3} ([A-Z\s]+) (.+)', r'\2', new_item)
 
         # this creates a list of with ONLY the lines (in a list!)
         if lines_stripped.get(new_m):
@@ -50,7 +50,7 @@ def prep():
         t_name = re.sub(r'[\+$]{7} .+', '', t_name)
         t_genre = re.sub(r'.+ (\[)', r'\1', new_item)
 
-        movies_list.append({'movie': t_nr, 'name': t_name, 'year': t_year, 'genres': t_genre, 'dialogue': lines_stripped[t_nr]})
+        movies_list.append({'movie': t_nr, 'name': t_name, 'year': t_year, 'genres': t_genre, 'dialogue': ' '.join(lines_stripped[t_nr])})
 
     print(movies_list[0])
 
