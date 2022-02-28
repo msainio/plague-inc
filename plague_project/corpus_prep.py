4a954c6fd01ed9236c506df5a39efab970a02dab
Ines Fröjdö <ines.frojdo@gmail.com>
2022-02-28 11:53:04 +0200

    #28 improving the corpus list

--- plague_project/corpus_prep.py
+++ plague_project/corpus_prep.py
@@ -1,4 +1,3 @@
-""" i wanna prepare the files appropriately for the task at hand """
 
 import re
 
@@ -15,48 +14,45 @@ ft = open(F_TITLES, "r", encoding="ISO-8859-2")
 titles = ft.read().splitlines()
 ft.close()
 
+def prep():
 # stripping unnecessary content into a file like: movie (m), charachter (NAME), line (str)
-lines_stripped = []
-for item in lines:
-    new_item = re.sub(r'[\+$]{7} ', '', item)
-    new_m = re.sub(r'L\d{1,6} u\d{1,4} (m\d{1,3}) .+', r'\1', new_item)
-    new_c = re.sub(r'L\d{1,6} u\d{1,4} m\d{1,3} (\w+) .+', r'\1', new_item)
-    new_l = re.sub(r'L\d{1,6} u\d{1,4} m\d{1,3} \w+ (.+)', r'\1', new_item)
-#    new_l = re.sub('\', '', new_l)
+    lines_stripped = {}
+    for item in lines:
+        new_item = re.sub(r'[\+$]{7} ', '', item)
+        new_m = re.sub(r'L\d{1,6} u\d{1,4} (m\d{1,3}) .+', r'\1', new_item)
+        new_c = re.sub(r'L\d{1,6} u\d{1,4} m\d{1,3} (\w+) .+', r'\1', new_item)
+        new_l = re.sub(r'L\d{1,6} u\d{1,4} m\d{1,3} \w+ (.+)', r'\1', new_item)
 
-    lines_stripped.append({'movie': new_m, 'character': new_c, 'line': new_l})
+        # this creates a list of with ONLY the lines (in a list!)
+        if lines_stripped.get(new_m):
+            lines_stripped[new_m].append(new_l)
+        else:
+            lines_stripped[new_m] = [new_l]
 
-#for j in lines_stripped[:10]:
-#    print(j, '\n')
 
-# new structure -> movie number (m), movie name (), year (), genres(['x', 'y'])
-titles_stripped = []
-for item in titles:
-    new_item = re.sub(r'[\+$]{7} ', '', item)
-    t_nr = re.sub(r'(m\d{1,3}).+', r'\1', new_item)
-    t_name = re.sub(r'm\d{1,3} [\+$]{7} (.+) ', r'\1', item)
-    t_year = re.sub(r'.+ [\+$]{7} (\d{4}) .+', r'\1', t_name)
-    t_name = re.sub(r'[\+$]{7} .+', '', t_name)
-    t_genre = re.sub(r'.+ (\[)', r'\1', new_item)
+    """ if you want the characters and lines (in a dictionary) use this code! """
+        #if lines_stripped.get(new_m):
+        #    lines_stripped[new_m].append({'character': new_c, 'line': new_l})
+        #else:
+        #    lines_stripped[new_m] = [new_l]
 
-    titles_stripped.append({'movie': t_nr, 'name': t_name, 'year': t_year, 'genres': t_genre, 'dialogue': []})
 
-#for i in titles_stripped[:10]:
-#    print(i, '\n')
-combined = []
+        #print(lines_stripped['m3'])
 
-for i in titles_stripped:
-    for j in lines_stripped:
-        if i['movie'] == j['movie']:
-            break
-#            i['dialogue'] += j['line']
 
-print(titles_stripped[:1])
+    # new structure -> movie number (m), movie name (), year (), genres(['x', 'y'])
+    movies_list = []
+    for item in titles:
+        new_item = re.sub(r'[\+$]{7} ', '', item)
+        t_nr = re.sub(r'(m\d{1,3}).+', r'\1', new_item)
+        t_name = re.sub(r'm\d{1,3} [\+$]{7} (.+) ', r'\1', item)
+        t_year = re.sub(r'.+ [\+$]{7} (\d{4}) .+', r'\1', t_name)
+        t_name = re.sub(r'[\+$]{7} .+', '', t_name)
+        t_genre = re.sub(r'.+ (\[)', r'\1', new_item)
 
-"""
-        for i, (score, doc_idx) in enumerate(ranked_scores_and_doc_ids):
-            for x in range(len(list)):
-                if content_list[doc_idx] in list[x]['innehåll']:
-                    list[x]['ranked value'] += str(score)
-                    matches.append(list[x])
-"""
+        movies_list.append({'movie': t_nr, 'name': t_name, 'year': t_year, 'genres': t_genre, 'dialogue': lines_stripped[t_nr]})
+
+    print(movies_list[0])
+
+
+prep()
